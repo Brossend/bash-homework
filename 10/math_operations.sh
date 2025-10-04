7ebc34b6a996a16803906f91af703ba50f775e45
@ilyamtsev <ilyamtsev@c-and-u.co>
2025-10-05 01:05:19 +0500

    feat(solution): Решение задачи #10

---
+++ 10/math_operations.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+add() {
+    echo $(( $1 + $2 ))
+}
+
+subtract() {
+    echo $(( $1 - $2 ))
+}
+
+multiply() {
+    echo $(( $1 * $2 ))
+}
+
+divide() {
+    if [ "$2" -eq 0 ]; then
+        echo "Ошибка: деление на ноль!"
+    else
+        echo $(( $1 / $2 ))
+    fi
+}
+
+read -p "Введите первое число: " num1
+read -p "Введите второе число: " num2
+read -p "Выберите операцию (+, -, *, /): " op
+
+case "$op" in
+    +) result=$(add $num1 $num2) ;;
+    -) result=$(subtract $num1 $num2) ;;
+    \*) result=$(multiply $num1 $num2) ;;
+    /) result=$(divide $num1 $num2) ;;
+    *) echo "Неизвестная операция"; exit 1 ;;
+esac
+
+echo "Результат: $result"
